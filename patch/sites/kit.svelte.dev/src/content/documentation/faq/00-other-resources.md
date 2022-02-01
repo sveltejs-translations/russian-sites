dbf75f2f8a5bfc14ffd05f78bc9587fd8b5cbcc1
AlexxNB <iam@alexxnb.ru>
2022-02-01 14:14:00 +0300

    Add Svelte site and fix translations

--- patch/sites/kit.svelte.dev/src/content/documentation/faq/00-other-resources.md
+++
@@ -1,5 +0,0 @@
----
-question: Другие источники
----
-
-Пожалуйста, смотрите [svelte FAQ](https://svelte.dev/faq) и [`vite-plugin-svelte` FAQ](https://github.com/sveltejs/vite-plugin-svelte/blob/main/DOCS/FAQ.MD), а также ответы на вопросы про эти библиотеки.
--- scripts/dev.js
+++ scripts/dev.js
@@ -1,16 +1,20 @@
 const watch = require('node-watch');
-const KIT = process.argv.includes('--kit');
 const child = require('child_process');
+
+const KIT = process.argv.includes('--kit');
+const SVELTE = process.argv.includes('--svelte');
 
 let dir;
 if(KIT) dir = '__BUILD/sites/kit.svelte.dev';
+if(SVELTE) dir = '__BUILD/sites/svelte.dev';
 
 runApply();
 
 const site = child.spawn('npm', ['run', 'dev'], {
     cwd: dir,
     stdio: ['ignore', 'inherit', 'inherit'],
-    shell: true
+    shell: true,
+    env: {...process.env, DOCS_PREVIEW:'true'}
 });
 
 process.on('SIGTERM', ()=>site.kill(0));
@@ -21,7 +25,6 @@ watch('patch',{recursive:true},runApply);
 function runApply(){
     try{
         child.execSync('npm run apply');
-        if(KIT) child.execSync('npm run fetch:kit');
     }catch{
         console.error('Something wrong in patch directory')
     }
