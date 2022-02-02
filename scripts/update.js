8caaf5f0d4456f6b90351e267efccbda14db25d1
AlexxNB <iam@alexxnb.ru>
2022-02-02 13:08:05 +0300

    Add --no-frozen-lockfile

--- scripts/update.js
+++ scripts/update.js
@@ -2,4 +2,4 @@ const {runSync} = require('./lib');
 
 runSync('npm run download');
 runSync('npm run apply');
-runSync('npx pnpm install','./__BUILD');
+runSync('npx pnpm install --no-frozen-lockfile','./__BUILD');
