643906b15c4c427d653250687f08ef34c73aa550
AlexxNB <iam@alexxnb.ru>
2022-02-02 15:07:34 +0300

    ooops

--- scripts/build.js
+++ scripts/build.js
@@ -4,15 +4,15 @@ const path = require('path');
 
 const DIR = getSiteDir();
 
-// runSync('npm run update');
+runSync('npm run update');
 if(getProject() === 'kit'){
   runSync('npx pnpm run build',DIR);
   writeFileSync(path.join(DIR,'build','package.json'),JSON.stringify({type:'module'}));
 }else{
- /* runSync('npx pnpm run build',DIR,{
+  runSync('npx pnpm run build',DIR,{
     SUPABASE_URL:'no',
     SUPABASE_KEY:'no',
-  });*/
+  });
   writeFileSync(path.join(DIR,'build','package.json'),JSON.stringify({type:'module'}));
   runSync('npm i @supabase/supabase-js cookie devalue do-not-zip flru marked dotenv',path.join(DIR,'build'));
 }
