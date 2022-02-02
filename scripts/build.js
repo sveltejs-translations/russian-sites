831650ed7023be56b284f1e97026e87f8584a88e
AlexxNB <iam@alexxnb.ru>
2022-02-02 15:06:15 +0300

    make svelte deployment

--- scripts/build.js
+++ scripts/build.js
@@ -1,16 +1,18 @@
 const {runSync,getSiteDir,getProject} = require('./lib');
-const {writeFileSync} = require('fs');
+const {writeFileSync,cpSync,rmSync} = require('fs');
 const path = require('path');
 
 const DIR = getSiteDir();
 
-runSync('npm run update');
-runSync('npx pnpm run build',DIR,
-  getProject() === 'kit' ? {} :
-  {
+// runSync('npm run update');
+if(getProject() === 'kit'){
+  runSync('npx pnpm run build',DIR);
+  writeFileSync(path.join(DIR,'build','package.json'),JSON.stringify({type:'module'}));
+}else{
+ /* runSync('npx pnpm run build',DIR,{
     SUPABASE_URL:'no',
     SUPABASE_KEY:'no',
-  }
-);
-
-writeFileSync(path.join(DIR,'build','package.json'),JSON.stringify({type:'module'}));
+  });*/
+  writeFileSync(path.join(DIR,'build','package.json'),JSON.stringify({type:'module'}));
+  runSync('npm i @supabase/supabase-js cookie devalue do-not-zip flru marked dotenv',path.join(DIR,'build'));
+}
--- scripts/update.js
+++ scripts/update.js
@@ -1,4 +1,5 @@
 const {runSync} = require('./lib');
+const path = require('path');
 
 runSync('npm run download');
 runSync('npm run apply');
